a9e62c89c754dded0b3b53129bb02be75be422b1
YeThura-424 <rathanpoudle.26@gmail.com>
2021-12-22 21:18:41 +0630

    wip

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,20 @@
-<?php
+    <?php
+    namespace Tests\Feature;
 
-namespace Tests\Feature;
+    use Illuminate\Foundation\Testing\RefreshDatabase;
+    use Tests\TestCase;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    class ExampleTest extends TestCase
     {
-        $response = $this->get('/');
+        /**
+         * A basic test example.
+         *
+         * @return void
+         */
+        public function test_example()
+        {
+            $response = $this->get('/');
 
-        $response->assertStatus(200);
+            $response->assertStatus(200);
+        }
     }
-}
