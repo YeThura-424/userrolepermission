8c3ab4c8b1d647c73cf02838b6ecafbca3dee640
YeThura-424 <rathanpoudle.26@gmail.com>
2022-01-21 21:35:39 +0630

    user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,44 +1,44 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-use Carbon\Carbon;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+    use Carbon\Carbon;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateUsersTable extends Migration
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->date('register_at')->default(Carbon::now()); //carbon::now is used to generate current/today date
-            $table->string('profile')->nullable();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('username');
-            $table->string('password');
-            $table->string('phone');
-            $table->string('address');
-            $table->string('status')->default(0);
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->date('register_at')->default(Carbon::now()); //carbon::now is used to generate current/today date
+                $table->string('profile')->nullable();
+                $table->string('first_name');
+                $table->string('last_name');
+                $table->string('email')->unique();
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('username');
+                $table->string('password');
+                $table->string('phone');
+                $table->string('address');
+                $table->string('status')->default(0);
+                $table->rememberToken();
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('users');
+        }
     }
-}
