560b9c10e955af17775ce742231046db323e5d1d
YeThura-424 <rathanpoudle.26@gmail.com>
2022-01-25 22:40:52 +0630

    category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,17 +1,16 @@
-<?php
+<?php 
+namespace App\Models; 
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+  use Illuminate\Database\Eloquent\Factories\HasFactory;
+  use Illuminate\Database\Eloquent\Model;
+  use Illuminate\Database\Eloquent\SoftDeletes;
 
 
-class Category extends Model
-{
-    use HasFactory,SoftDeletes;
+  class Category extends Model
+  {
+      use HasFactory,SoftDeletes;
 
-    protected $fillable = [
-		'name','category_code','photo'
-	];
-}
+      protected $fillable = [
+      'name','category_code','photo'
+    ];
+  }
--- database/migrations/2022_01_25_043001_create_categories_table.php
+++ database/migrations/2022_01_25_043001_create_categories_table.php
@@ -1,35 +1,35 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateCategoriesTable extends Migration
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('category_code');
-            $table->string('photo');
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('categories', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('category_code');
+                $table->string('photo');
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('categories');
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('categories');
+        }
     }
-}
