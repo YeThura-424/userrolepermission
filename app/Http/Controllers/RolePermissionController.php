2f17cea53a027adacdfc71c1efcf4fb2c37c5eaf
YeThura-424 <rathanpoudle.26@gmail.com>
2022-03-23 22:49:07 +0630

    rolepermissin add, edit

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -14,23 +14,23 @@ class RolePermissionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $roles = Role::all();
-        return view('backend.rolepermission.list', compact('roles'));
-    }
+        public function index()
+        {
+            $roles = Role::all();
+            return view('backend.rolepermission.list', compact('roles'));
+        }
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        $permissions = Permission::all();
-        // dd($permissions);
-        return view('backend.rolepermission.new', compact('permissions'));
-    }
+        public function create()
+        {
+            $permissions = Permission::all();
+            // dd($permissions);
+            return view('backend.rolepermission.new', compact('permissions'));
+        }
 
     /**
      * Store a newly created resource in storage.
@@ -38,44 +38,40 @@ class RolePermissionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        // dd($request);
-        $rolename = $request->name;
-        $role = new Role;
-        $role->name = $rolename;
-        $role->save();
+        public function store(Request $request)
+        {
+            // dd($request);
+            $rolename = $request->name;
+            $role = new Role;
+            $role->name = $rolename;
+            $role->save();
 
-        $permissions_name = $request->permissionname;
-        $permission_id = $request->permission_id;
-        $creates = $request->create;
-        $reads = $request->read;
-        $updates = $request->update;
-        $deletes = $request->delete;
+            $permissions_name = $request->permissionname;
+            $permission_id = $request->permission_id;
+            $creates = $request->create;
+            $reads = $request->read;
+            $updates = $request->update;
+            $deletes = $request->delete;
 
-        foreach ($permission_id as $id) {
-            $permission = $permissions_name[$id];
-            $create = $creates[$id];
-            $read = $reads[$id];
-            $update = $updates[$id];
-            $delete = $deletes[$id];
-            if ($create == 'yes' || $read == 'yes' || $update == 'yes' || $delete == 'yes') {
-                $role->permissions()->attach($id, [
-                    'create' => $create,
-                    'read' => $read,
-                    'update' => $update,
-                    'delete' => $delete
-                ]);
+            foreach ($permission_id as $id) {
+                $permission = $permissions_name[$id];
+                $create = $creates[$id];
+                $read = $reads[$id];
+                $update = $updates[$id];
+                $delete = $deletes[$id];
+                if ($create == 'yes' || $read == 'yes' || $update == 'yes' || $delete == 'yes') {
+                    $role->permissions()->attach($id, [
+                        'create' => $create,
+                        'read' => $read,
+                        'update' => $update,
+                        'delete' => $delete
+                    ]);
+                }
+
             }
 
-            // $role->permissions()->attach($id, ['read' => $read]);
-            // $role->permissions()->attach($id, ['update' => $update]);
-            // $role->permissions()->attach($id, ['delete' => $delete]);
-            // $role->syncPermissions($permission);
+            return redirect()->route('rolepermission.index')->with("successMsg", 'New RolePermission is ADDED in your database');
         }
-
-        return redirect()->route('rolepermission.index')->with("successMsg", 'New RolePermission is ADDED in your database');
-    }
 
     /**
      * Display the specified resource.
@@ -94,27 +90,27 @@ class RolePermissionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        // dd($id);
-        $role = Role::find($id);
-        $permissions = Permission::all();
-        $rolepermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
-            ->pluck('role_has_permissions.permission_id')->all();
-        $create = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
-            ->pluck('role_has_permissions.create', 'role_has_permissions.permission_id')->all();
-        $read = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
-            ->pluck('role_has_permissions.read', 'role_has_permissions.permission_id')->all();
-        $update = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
-            ->pluck('role_has_permissions.update', 'role_has_permissions.permission_id')->all();
-        $delete = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
-            ->pluck('role_has_permissions.delete', 'role_has_permissions.permission_id')->all();
-        // dd($rolepermissions, $create);
+        public function edit($id)
+        {
+            // dd($id);
+            $role = Role::find($id);
+            $permissions = Permission::all();
+            $rolepermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+                ->pluck('role_has_permissions.permission_id')->all();
+            $create = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+                ->pluck('role_has_permissions.create', 'role_has_permissions.permission_id')->all();
+            $read = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+                ->pluck('role_has_permissions.read', 'role_has_permissions.permission_id')->all();
+            $update = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+                ->pluck('role_has_permissions.update', 'role_has_permissions.permission_id')->all();
+            $delete = DB::table("role_has_permissions")->where("role_has_permissions.role_id", $id)
+                ->pluck('role_has_permissions.delete', 'role_has_permissions.permission_id')->all();
+            // dd($rolepermissions, $create);
 
 
 
-        return view('backend.rolepermission.edit', compact('role', 'permissions', 'rolepermissions', 'create', 'update', 'read', 'delete'));
-    }
+            return view('backend.rolepermission.edit', compact('role', 'permissions', 'rolepermissions', 'create', 'update', 'read', 'delete'));
+        }
 
 
     /**
@@ -124,45 +120,45 @@ class RolePermissionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        // dd($id);
-        // dd($request);
-        $name = $request->name;
-        $role = Role::find($id);
-        $role->name = $name;
-        $role->save();
+        public function update(Request $request, $id)
+        {
+            // dd($id);
+            // dd($request);
+            $name = $request->name;
+            $role = Role::find($id);
+            $role->name = $name;
+            $role->save();
 
-        $permissions_name = $request->permissionname;
-        $permission_ids = $request->permission_id;
-        $creates = $request->create;
-        $reads = $request->read;
-        $updates = $request->update;
-        $deletes = $request->delete;
+            $permissions_name = $request->permissionname;
+            $permission_ids = $request->permission_id;
+            $creates = $request->create;
+            $reads = $request->read;
+            $updates = $request->update;
+            $deletes = $request->delete;
 
-        // dd($creates, $reads, $updates, $deletes, $permission_id);
+            // dd($creates, $reads, $updates, $deletes, $permission_id);
 
-        foreach ($permission_ids as $ids) {
-            $permission_id = $permission_ids[$ids];
-            $create = $creates[$ids];
-            $read = $reads[$ids];
-            $update = $updates[$ids];
-            $delete = $deletes[$ids];
-            if ($create == 'yes' || $read == 'yes' || $update == 'yes' || $delete == 'yes') {
-                DB::table('role_has_permissions')->where([
-                    ['role_id', $id],
-                    ['permission_id', $permission_id]
-                ])->update([
-                    // 'permission_id' => $permission_id,
-                    'create' => $create,
-                    'read' => $read,
-                    'update' => $update,
-                    'delete' => $delete,
-                ]);
+            foreach ($permission_ids as $ids) {
+                $permission_id = $permission_ids[$ids];
+                $create = $creates[$ids];
+                $read = $reads[$ids];
+                $update = $updates[$ids];
+                $delete = $deletes[$ids];
+                if ($create == 'yes' || $read == 'yes' || $update == 'yes' || $delete == 'yes') {
+                    DB::table('role_has_permissions')->where([
+                        ['role_id', $id],
+                        ['permission_id', $permission_id]
+                    ])->update([
+                        // 'permission_id' => $permission_id,
+                        'create' => $create,
+                        'read' => $read,
+                        'update' => $update,
+                        'delete' => $delete,
+                    ]);
+                }
             }
+            return redirect()->route('rolepermission.index')->with("successMsg", 'Existing RolePermission is UPDATED in your database');
         }
-        return redirect()->route('rolepermission.index')->with("successMsg", 'Existing RolePermission is UPDATED in your database');
-    }
 
     /**
      * Remove the specified resource from storage.
--- database/migrations/2022_03_23_092817_add_create_to_role_has_permissions_table.php
+++ database/migrations/2022_03_23_092817_add_create_to_role_has_permissions_table.php
@@ -1,35 +1,35 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class AddCreateToRoleHasPermissionsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class AddCreateToRoleHasPermissionsTable extends Migration
     {
-        Schema::table('role_has_permissions', function (Blueprint $table) {
-            $table->string('create');
-            $table->string('read');
-            $table->string('update');
-            $table->string('delete');
-        });
-    }
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::table('role_has_permissions', function (Blueprint $table) {
+                $table->string('create');
+                $table->string('read');
+                $table->string('update');
+                $table->string('delete');
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('role_has_permissions', function (Blueprint $table) {
-            //
-        });
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::table('role_has_permissions', function (Blueprint $table) {
+                //
+            });
+        }
     }
-}
