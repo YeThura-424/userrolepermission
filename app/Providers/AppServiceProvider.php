f19e0470a130b67091d8aaad2479defbd85e16f0
YeThura-424 <rathanpoudle.26@gmail.com>
2022-04-05 20:29:40 +0630

    permission

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,7 +18,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap any application services.
      *
@@ -27,29 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // view()->composer(
-        //     '*',
-        //     function ($view) {
-        //         $permissions = Permission::all();
-        //         // dd($permissions);
-        //         foreach ($permissions as $permission) {
-        //             // dd($permission);
-        //             $permission_id = $permission->id;
-        //             // dd($permission_id);
-        //             if (Auth::check()) {
-        //                 $user = Auth::user();
-        //                 // dd($user);
-        //                 $user_id = $user->id;
-        //                 // dd($user_id);
-        //                 $categoryPermissions = DB::table('model_has_permissions')->where([
-        //                     ['model_id', $user_id],
-        //                     ['permission_id', $permission_id]
-        //                 ])->get();
-        //                 // dd($categoryPermissions);
-        //                 $view->with('data', $categoryPermissions);
-        //             }
-        //         }
-        //     }
-        // );
+        //
     }
 }
