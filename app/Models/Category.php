bce627bf3b0aa8133e329583111d5bdc6d74d3e6
YeThura-424 <rathanpoudle.26@gmail.com>
2022-07-08 12:12:00 +0630

    category models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,16 +1,19 @@
-<?php 
-namespace App\Models; 
+<?php
 
-  use Illuminate\Database\Eloquent\Factories\HasFactory;
-  use Illuminate\Database\Eloquent\Model;
-  use Illuminate\Database\Eloquent\SoftDeletes;
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
-  class Category extends Model
-  {
-      use HasFactory,SoftDeletes;
+class Category extends Model
+{
+    use HasFactory, SoftDeletes;
 
-      protected $fillable = [
-      'name','category_code','photo'
+    protected $fillable = [
+        'name',
+        'category_code',
+        'photo'
     ];
-  }
+}
