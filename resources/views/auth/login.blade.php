0c917161e29367b267e28e24bda5f500d7c1159b
YeThura-424 <rathanpoudle.26@gmail.com>
2023-08-17 20:42:25 +0630

    login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,98 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>YTR-ERP</title>
-    <link href="{{asset('appstructure/css/styles.css')}}" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>YTR-ERP</title>
+        <link href="{{asset('appstructure/css/styles.css')}}" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
 
-<body>
-    <div id="layoutAuthentication">
-        <div id="layoutAuthentication_content">
-            <main>
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-5">
-                            <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                <div class="card-header">
-                                    <h3 class="text-center font-weight-light my-4">Login</h3>
-                                </div>
-                                <div class="card-body">
-                                    @if(session('successMsg') != NULL)
-                                    <div class="text-danger" role="alert">
-                                        {{ session('successMsg') }}
+    <body>
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header">
+                                        <h3 class="text-center font-weight-light my-4">Login</h3>
                                     </div>
-                                    @endif
-                                    <form method="POST" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputEmail" type="text" placeholder="name@example.com" name="username" value="{{ old('email') }}" {{-- autofocus="false" --}} />
-                                            <label for="inputEmail">User Name</label>
-                                            @error('username')
-                                            <span class="text-danger" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <div class="form-floating mb-3">
-                                            <input class="form-control" id="inputPassword" type="password" placeholder="Password" name="password" />
-                                            <label for="inputPassword">Password</label>
-                                            @error('password')
-                                            <span class="text-danger" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                            @enderror
-                                        </div>
-                                        <div class="form-check mb-3">
-                                            <input class="form-check-input" id="inputRememberPassword" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }} />
-                                            <label class="form-check-label" for="inputRememberPassword">Remember Me</label>
-                                        </div>
-                                        @if(session('errorMsg') != NULL)
+                                    <div class="card-body">
+                                        @if(session('successMsg') != NULL)
                                         <div class="text-danger" role="alert">
-                                            {{ session('errorMsg') }}
+                                            {{ session('successMsg') }}
                                         </div>
                                         @endif
-                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                            <a class="small" href="password.html">Forgot Password?</a>
-                                            <button type="submit" class="btn btn-primary">Login</button>
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="text" placeholder="name@example.com" name="username" value="{{ old('email') }}" {{-- autofocus="false" --}} />
+                                                <label for="inputEmail">User Name</label>
+                                                @error('username')
+                                                <span class="text-danger" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password" placeholder="Password" name="password" />
+                                                <label for="inputPassword">Password</label>
+                                                @error('password')
+                                                <span class="text-danger" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input class="form-check-input" id="inputRememberPassword" name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }} />
+                                                <label class="form-check-label" for="inputRememberPassword">Remember Me</label>
+                                            </div>
+                                            @if(session('errorMsg') != NULL)
+                                            <div class="text-danger" role="alert">
+                                                {{ session('errorMsg') }}
+                                            </div>
+                                            @endif
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">Forgot Password?</a>
+                                                <button type="submit" class="btn btn-primary">Login</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div>
+                                            <p>Login Info</p>
+                                            <p>Username : rathan406</p>
+                                            <p>Password : rathan406</p>
                                         </div>
-                                    </form>
-                                </div>
-                                <div class="card-footer text-center py-3">
-                                    <div><p>Login Info</p>
-                                        <p>Username : rathan406</p>
-                                        <p>Password : rathan406</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </main>
-        </div>
-        <div id="layoutAuthentication_footer">
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2021</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </footer>
+                </footer>
+            </div>
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="{{asset('appstructure/js/scripts.js')}}"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{asset('appstructure/js/scripts.js')}}"></script>
+    </body>
 
-</html>
+    </html>
